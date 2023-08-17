9a728791bd72480ac423ccb08c9646d38caba82f
Jordan Reger <mail@jordanreger.com>
2023-08-17 17:52:28 -0400

    add listRecords, describeRepo, and getRecord

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std/http/server.ts";
-import { getDID } from "./did.ts";
+import { getDID } from "../lib/getDID.ts";
 
 const headers = { headers: { "Content-Type": "application/json" } };
 
@@ -7,7 +7,48 @@ async function handler (req: Request): Response {
 	const url = new URL(req.url),
 				pathname = url.pathname,
 				params = url.searchParams;
+
+	// com.atproto.repo
 	
+	// .describeRepo
+	if (pathname === "/xrpc/com.atproto.repo.describeRepo") {
+		const repo = params.get("repo");
+		if (repo) {
+			const res = await fetch(`https://bsky.social/xrpc/com.atproto.repo.describeRepo?repo=${repo}`).then(res => res.json());
+			return new Response(JSON.stringify(res), headers);
+		} else {
+			return new Response(JSON.stringify({ "error": "InvalidRequest", "message": "Error: Params must have the property \"repo\"" }), headers);
+		}
+	}
+	// .listRecords
+	if (pathname === "/xrpc/com.atproto.repo.listRecords") {
+		const repo = params.get("repo"), collection = params.get("collection");
+		if (!repo) {
+			return new Response(JSON.stringify({ "error": "InvalidRequest", "message": "Error: Params must have the property \"repo\"" }), headers);
+		} else if (!collection) {
+			return new Response(JSON.stringify({ "error": "InvalidRequest", "message": "Error: Params must have the property \"repo\"" }), headers);
+		} else {
+			const res = await fetch(`https://bsky.social/xrpc/com.atproto.repo.listRecords?repo=${repo}&collection=${collection}`).then(res => res.json());
+				return new Response(JSON.stringify(res), headers);
+		}
+	}
+	// .getRecord
+	if (pathname === "/xrpc/com.atproto.repo.getRecord") {
+		const repo = params.get("repo"), collection = params.get("collection"), rkey = params.get("rkey");
+		if (!repo) {
+			return new Response(JSON.stringify({ "error": "InvalidRequest", "message": "Error: Params must have the property \"repo\"" }), headers);
+		} else if (!collection) {
+			return new Response(JSON.stringify({ "error": "InvalidRequest", "message": "Error: Params must have the property \"repo\"" }), headers);
+		} else if (!rkey) {
+			return new Response(JSON.stringify({ "error": "InvalidRequest", "message": "Error: Params must have the property \"rkey\"" }), headers);
+
+		} else {
+			const res = await fetch(`https://bsky.social/xrpc/com.atproto.repo.getRecord?repo=${repo}&collection=${collection}&rkey=${rkey}`).then(res => res.json());
+				return new Response(JSON.stringify(res), headers);
+		}
+	}
+
+	// com.atproto.identity
 	if (pathname === "/xrpc/com.atproto.identity.resolveHandle") {
 		const handle = params.get("handle");
 		const did = await getDID(handle);
